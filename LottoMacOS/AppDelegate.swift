23b7944cedfbbe4b6c5ff6bd826e782ae737090e
Michał Moskała <moskala.michal@gmail.com>
2018-10-26 09:40:23 +0300

    Configured app to be a deamon, added system item

--- LottoMacOS/AppDelegate.swift
+++ LottoMacOS/AppDelegate.swift
@@ -12,10 +12,15 @@ import LottoAPI
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var menu: NSMenu!
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        statusItem.title = "Lotto"
+        statusItem.menu = menu
+        
         let url = URL(string: "http://serwis.mobilotto.pl")!
         let client = LottoAPI.makeClient(baseURL: url)
         client.getNewestResults { (response) in
@@ -31,7 +36,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
+    @IBAction func quitSelected(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
+    
 }
 
